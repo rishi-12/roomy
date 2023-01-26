409b1e16ce64b6d29d51f8b8849b6f7bdbcc01f6
mighil31 <mighildath@gmail.com>
2023-01-26 21:45:04 +0530

    create chat component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import Chat from "./components/Chat/Chat";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<NavBar />}></Route>
+        <Route path="/messaging" element={<Chat />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Chat/Chat.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import NavBar from "../NavBar";
+
+export default function Chat() {
+  return (
+    <>
+      <NavBar />
+      Chat
+    </>
+  );
+}
