1140b046bab60a64880b44baa7f854f43a42ba4f
mighil31 <mighil@GLaDOS.localdomain>
2023-01-26 19:53:00 +0530

    integrate prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import NavBar from './components/NavBar';
+import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
     </div>
   );
 }
