b1ef91521ada3ed077816d4bf8eeaf6f3d714d0a
mighil31 <mighildath@gmail.com>
2023-01-28 00:34:26 +0530

    change container in feed component

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,7 +1,4 @@
 import * as React from "react";
-import NavBar from "../NavBar";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import { styleConstants } from "../../constants/styleConstants";
 import "../../css/chat.scss";
--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
+import { styleConstants } from "../../constants/styleConstants";
 
 export default function Feed() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
+      <Container
+        sx={{ bgcolor: styleConstants.bg_color, height: "100vh", pt: "2em" }}
+        maxWidth={false}
+        disableGutters
+      >
         <h1>Feed</h1>
       </Container>
     </React.Fragment>
